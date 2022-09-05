d7b891aa436aff590f351509115fabeb11fa4deb
Gabri <gabramsua@gmail.com>
2022-09-05 19:27:19 +0200

    routing clasificacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarioComponent } from './components/calendario/calendario.component';
+import { ClasificacionComponent } from './components/clasificacion/clasificacion.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { PuntuacionesDeCataComponent } from './components/puntuaciones-de-cata/puntuaciones-de-cata.component';
 import { PuntuacionesComponent } from './components/puntuaciones/puntuaciones.component';
-import { PuntuarComponent } from './components/puntuar/puntuar.component';
 import { VerCataComponent } from './components/ver-cata/ver-cata.component';
 
 const routes: Routes = [
@@ -13,7 +13,7 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'verCata', component: VerCataComponent},
   {path: 'calendario', component: CalendarioComponent},
-  {path: 'puntuar', component: PuntuarComponent},
+  {path: 'clasificacion', component: ClasificacionComponent},
   {path: 'puntuaciones-de-cata', component: PuntuacionesDeCataComponent},
   {path: 'puntuaciones', component: PuntuacionesComponent},
 ];
