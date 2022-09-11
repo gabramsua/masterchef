1c14f44bf6205cb6ca6c271e1ccf6b51298eeea8
Gabri <gabramsua@gmail.com>
2022-09-11 18:43:13 +0200

    Show PuntuacionesDeCata

--- src/app/components/puntuaciones-de-cata/puntuaciones-de-cata.component.ts
+++ src/app/components/puntuaciones-de-cata/puntuaciones-de-cata.component.ts
@@ -16,6 +16,18 @@ export class PuntuacionesDeCataComponent implements OnInit{
   jueces!: User[];
   catasRealizadasDelUsuario: Cata[] = [];
   allPuntuacionesDelUsuario: any[] = [];
+  allPuntuacionesDelUsuarioCata1: any[] = [];
+  allPuntuacionesDelUsuarioCata2: any[] = [];
+
+  
+  platos: any[] = [
+    {viewValue: '', value: '100'},
+    {viewValue: 'Entrante', value: '0'},
+    {viewValue: 'Principal', value: '1'},
+    {viewValue: 'Postre', value: '2'},
+  ];
+  selectedOrden = this.platos[0].value;
+  selectedOrden2 = this.platos[0].value;
 
   constructor(public _service: AuthService,) { }
 
@@ -32,19 +44,58 @@ export class PuntuacionesDeCataComponent implements OnInit{
     this._service.puntuaciones$.subscribe( puntuaciones => {
       puntuaciones.sort((a, b) => (moment(a.id, 'DD-MM-YYYY').toDate().valueOf() > moment(b.id, 'DD-MM-YYYY').toDate().valueOf()) ? 1 : -1)
       this.allPuntuacionesDelUsuario = puntuaciones.filter( elem => elem.telefono == this.cocinero.telefono)
+      console.log(this.allPuntuacionesDelUsuario)
     })
     
     // 0. Guardar la fecha de la cata al establecer una fecha
     // 1. Traerse al user y sus fechas de catas
     // 2. Get Puntuaciones para cada fecha
+
+    // YO CREO QUE YA SE PUEDEN MOSTRAR LAS PUNTUACIONES
     
   }
 
-  // getAllCatas() {
-  //   this._service.getAll(Constants.END_POINTS.CATAS)
-  // }
   getAllPuntuaciones() {
     this._service.getAll(Constants.END_POINTS.PUNTUACIONES)
+  }
+
+  verCata(i: number){
+    console.log('Click ver Cata', i)
+  }
+  getAllPuntuacionesDelUsuario(index: number) {
+    return this.allPuntuacionesDelUsuario[index];
+  }
+  selectPlato(index: number, plato: string) {
+
+    if(parseInt(plato) != 100) {
+      if(index == 0) {
+        this.selectedOrden =  this.platos[parseInt(plato)].value
+        this.allPuntuacionesDelUsuarioCata1 = [];
+      }
+      if(index == 1) {
+        this.selectedOrden2 =  this.platos[parseInt(plato)].value
+        this.allPuntuacionesDelUsuarioCata2 = [];
+      }
+
+      const array = {...this.allPuntuacionesDelUsuario[index]}
+      
+      for (const property in array) {
+        this.jueces.map((juez:User) => {
+          if(property == juez.telefono) {
+            if(index == 0) this.allPuntuacionesDelUsuarioCata1.push(array[property][parseInt(plato)]);
+            if(index == 1) this.allPuntuacionesDelUsuarioCata2.push(array[property][parseInt(plato)]);
+          }
+        })
+      }
+    } else {
+      
+      if(index == 0) {
+        this.allPuntuacionesDelUsuarioCata1 = [];
+      }
+      if(index == 1) {
+        this.allPuntuacionesDelUsuarioCata2 = [];
+      }
+    }
   }
 
 }
